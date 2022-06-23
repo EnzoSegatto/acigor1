1bf0f48305b893fb2272fb441d9333b0437abcf2
EnzoSegatto <84516332+EnzoSegatto@users.noreply.github.com>
2022-06-23 13:08:39 -0300

    Add files via upload

---
+++ ac/lanches.js
@@ -0,0 +1,35 @@
+
+class McLancheAlegre{
+constructor(carne, queijo, fruta, batatapequena){
+    this.carne = carne;
+    this.queijo = queijo;
+    this.fruta = fruta;
+    this.batatapequena = batatapequena;
+    }
+}
+
+class BigLanche{
+constructor(carne, queijo, alface, molhobacana, cebola, picles, batatamedia){
+    this.carne = carne;
+    this.queijo = queijo;
+    this.molhobacana = molhobacana;
+    this.alface = alface;
+    this.cebola = cebola;
+    this.picles = picles;
+    if 
+    }
+}
+class BigHen{
+    constructor(frango, queijo, picles,alface){
+        this.frango = frango;
+        this.queijo = queijo;
+        this.alface = alface;
+        this.picles = picles;
+    }   
+}
+
+class lanche extends McLancheAlegre,BigLanche,BigHen{
+    constructor(McLancheAlegre,BigLanche,BigHen){
+        this.combo = option;
+    }
+}
